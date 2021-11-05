fb5abf219ae1a16e12c2e60a9ed0c953cc3fa763
surfete <orlandolabbe@hotmail.com>
2021-11-05 18:10:59 -0300

    Editing the README to try out git add/commit

---
+++ FeriaDesktop/View/Applications.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FeriaDesktop.View
+{
+    /// <summary>
+    /// Lógica de interacción para Contracts.xaml
+    /// </summary>
+    public partial class Contracts : Window
+    {
+        public Contracts()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if ((string)e.Column.Header == "IdUsuario")
+            {
+                e.Cancel = true;
+            }
+            if ((string)e.Column.Header == "IdContrato")
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
